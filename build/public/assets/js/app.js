03ecb53c0f5f1e86bf88be8cc1ea4ec1fca90095
Jp Gary <jpgary@gmail.com>
2012-06-28 22:58:21 -0400

    more templating and scroll / navigation functionality
    added more of the copy into the template (twitter api call), created a
    functional scrollTop nav now so it follows the animation (needs work)
    as well as moves down to the right 'spot' on the page.

--- build/public/assets/js/app.js
+++ build/public/assets/js/app.js
@@ -1,60 +1,9 @@
-var NavView = Em.View.create({
-	templateName:"nav-template",
-	classNames:"navigation",
-
-	items:[
-		//{name:"HOME" , link:TopView }
-		{name:"SIC Group" , link:AboutView },
-		{name:"Formulas" , link:FormulasView },
-		{name:"Team" , link:TeamView },
-		{name:"Opportunities" , link:OpportunitiesView },
-		{name:"Contact" , link:ContactView }
-	],
-	/*init:function(){ this._super();},*/
-	
-	didInsertElement:function(){
-		$(".navigation-item").click(this.itemClicked);
-		resizeAll();
-
-	},
-
-	itemClicked:function(e){
-		Debug.trace(' ITEM CLICKED ' + $(this).data("link") );
-	},
-
-	scrollEvent:function(e, delta){
-		//Debug.trace(' scroll -- ' );
-		//Debug.trace( delta);
-		//?? resizeAll(); 
-
-		NavView.resize({width:$(window).width() , height:$(window).height() });
-	},
-
-	resize:function(obj){
-		var navcoordY = (TopView.$().height()-NavView.$().height());
-
-		var belowFold = ($(window).scrollTop() > navcoordY )? true : false; 
-
-		var pos = (belowFold == true )? 'fixed' : 'absolute';
-		var top = (belowFold == true )? 0 :  TopView.$().height()-NavView.$().height() ;
-		var left = (obj.width - NavView.$().width())/2;
-
-		NavView.$().css({
-			'position':pos,
-			'top':top,
-			'left':left
-		});
-		
-	}
-})
-
 var TopView = Em.View.create({
 	templateName:"container-template",
 	classNames:["top", "container"],
 		
 	childView:Em.View.extend({
 		templateName:"top-content",
-		name:"TOP INSERTED",
 		copy:copy.main
 	}),
 	didInsertElement:function(){
@@ -76,16 +25,41 @@ var AboutView = Em.View.create({
 	templateName:"container-template",
 	classNames:["about", "container"],
 	
-	childView:Em.View.extend({
+	childView:Em.View.create({
 		templateName:"about-content",
-		name:"ABOUT",
-		copy:copy.about
+		copy:copy.about,
+		
+		tweetTime:'',
+		tweet:'',
+		
+		didInsertElement:function(){
+			//load tweet message:
+			//https://dev.twitter.com/docs/api/1/get/statuses/user_timeline
+			$.getJSON(
+					"http://api.twitter.com/1/statuses/user_timeline.json?screen_name="+
+					this.copy.twitter.account+"&include_entities=0&trim_user=1&count=10"+
+					"&callback=?", //make it JSONP XMLHTTPRequest
+					function(data){
+						if( data.length > 0 ){
+							
+							var d = data[0].created_at;
+								d = d.replace("+0000 ", "");
+							var date = new Date(d);
+							
+							AboutView.childView.set("tweetTime" , date.toLocaleDateString() );
+							AboutView.childView.set("tweet", data[0].text );
+						}else{
+							$("#twitter-container").fadeOut();
+						}
+					}
+					);
+		}
 	}),
 
 	didInsertElement:function(){
-		child = this.childView.create().appendTo( this.$('.content') );
+		//child = this.childView.create().appendTo( this.$('.content') );
+		this.childView.appendTo( this.$('.content') );
   	},
-
   	resize:function(obj){}
 });
 
@@ -156,3 +130,89 @@ var ContactView = Em.View.create({
   	resize:function(obj){}
 });
 
+
+
+var NavView = Em.View.create({
+	templateName:"nav-template",
+	classNames:"navigation",
+
+	items:[
+		//{name:"HOME" , link:TopView }
+		{	name:"SIC Group", 
+			id:'about', 
+			view:AboutView
+		},
+		{
+			name:"Formulas", 
+			id:'formula', 
+			view:FormulasView 
+		},
+		{
+			name:"Team", 
+			id:'team',
+			view:TeamView 
+		},
+		{
+			name:"Opportunities", 
+			id:'opportunities', 
+			view:OpportunitiesView 
+		},
+		{
+			name:"Contact", 
+			id:'contact', 
+			view:ContactView 
+		}
+	],
+	/*init:function(){ this._super();},*/
+	
+	didInsertElement:function(){
+		$(".navigation-item").click(this.itemClicked);
+		resizeAll();
+
+	},
+
+	itemClicked:function(e){
+		Debug.trace(' ITEM CLICKED ' + $(this).data("link") );
+		for(var i=0; i < NavView.items.length; i++){
+			if( $(this).data("link") == NavView.items[i].id ){
+				//MATCH:
+				var view = NavView.items[i].view;
+				var val = view.$().offset().top;
+				
+				$('body,html').animate({
+									scrollTop:val
+									},
+									500,
+									resizeAll
+								);
+
+				break;
+			}
+		}
+	},
+
+	scrollEvent:function(e, delta){
+		//Debug.trace(' scroll -- ' );
+		//Debug.trace( delta);
+		//?? resizeAll(); 
+
+		NavView.resize({width:$(window).width() , height:$(window).height() });
+	},
+
+	resize:function(obj){
+		var navcoordY = (TopView.$().height()-NavView.$().height());
+
+		var belowFold = ($(window).scrollTop() > navcoordY )? true : false; 
+
+		var pos = (belowFold == true )? 'fixed' : 'absolute';
+		var top = (belowFold == true )? 0 :  TopView.$().height()-NavView.$().height() ;
+		var left = (obj.width - NavView.$().width())/2;
+
+		NavView.$().css({
+			'position':pos,
+			'top':top,
+			'left':left
+		});
+		
+	}
+})
--- build/public/assets/js/copy.js
+++ build/public/assets/js/copy.js
@@ -4,6 +4,15 @@ var copy = {
 		subtitle:"Read more about our formulas:"
 	},
 	about:{
+
+		twitter:{
+			account:"jepiga",
+			title:"Latest Tweet",
+			tweet:"",
+			time:"",
+			button:"Follow Us On Twitter"
+		},
+
 		body:[
 				"SIC Group is a 21st Century Public Diplomacy firm tailored to meet the growing demand for a holistic approach, fusing an unmatched international public policy capability with a tactical strategic communication toolbox.",
 				"SIC Group is a Washington, D.C. headquartered firm, which operates across emerging markets and frontier markets. SIC Group caters to the public and corporate sector alike. The synergy of corporate and public access in frontier markets enables us to apply unique methodologies to produce directly measurable results."
